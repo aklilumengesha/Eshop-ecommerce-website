80b3156a131008fdae68d1185caebb36866314f8
Aklilu Mengesha <aklilumengesha57@gmail.com>
2026-02-02 19:45:37 +0300

    addded the necesary domains to Next.js config

--- next.config.js
+++ next.config.js
@@ -2,7 +2,17 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["res.cloudinary.com", "www.w3.org", "images.unsplash.com"],
+    domains: [
+      "res.cloudinary.com",
+      "www.w3.org",
+      "images.unsplash.com",
+      "www.logo.wine",
+      "upload.wikimedia.org",
+      "logos-world.net",
+      "1000logos.net",
+      "seeklogo.com",
+      "purepng.com",
+    ],
   },
 };
 
