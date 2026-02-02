eabadf414cf7041c8745fdd117d3c84303475b9e
Aklilu Mengesha <aklilumengesha57@gmail.com>
2026-02-02 15:30:47 +0300

    fix the issue of import path

--- pages/api/seed-testimonials.js
+++ pages/api/seed-testimonials.js
@@ -1,5 +1,5 @@
 import { getServerSession } from 'next-auth/next';
-import { authOptions } from '../auth/[...nextauth]';
+import { authOptions } from './auth/[...nextauth]';
 import Testimonial from '@/models/Testimonial';
 import db from '@/utils/db';
 
